4f345d79ec9429833ca7a4b6d3e9d618e6468ad0
shravan vyawahare <shravanvy@gmail.com>
2019-06-15 08:54:06 +0530

    second commit

--- src/com/google/Account.java
+++ src/com/google/Account.java
@@ -1,9 +1,53 @@
 package com.google;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Account {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\group021\\Desktop\\Software\\chromedriver_win32\\chromedriver.exe");
+	
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp");
+		driver.manage().window().maximize();
+		
+		  driver.findElement(By.name("firstName")).sendKeys("shravan");
+		  Thread.sleep(2000);
+		  
+		  driver.findElement(By.name("lastName")).sendKeys("vyawahare");
+		  Thread.sleep(2000);
+		  
+		  driver.findElement(By.name("Username")).sendKeys("shr1.infoway");
+		  Thread.sleep(2000);
+		  
+		  driver.findElement(By.name("Passwd")).sendKeys("info@cdac");
+		  Thread.sleep(2000);
+		  
+		  driver.findElement(By.name("ConfirmPasswd")).sendKeys("info@cdac");
+		  Thread.sleep(2000);
+		  
+		  driver.findElement(By.xpath("//*[@id=\"accountDetailsNext\"]/span")).click();
+		  Thread.sleep(2000);
+		  
+		  driver.findElement(By.xpath("//*[@id=\"phoneNumberId\"]")).sendKeys("7770070868");
+		  Thread.sleep(2000);
+		  
+		  driver.findElement(By.xpath("//*[@id=\"gradsIdvPhoneNext\"]/span")).click();
+		  Thread.sleep(2000);
+		  
+		  driver.close();
+		  
+		  
+		
+		
+		
+		
+		
+	
+		
 
 	}
 
